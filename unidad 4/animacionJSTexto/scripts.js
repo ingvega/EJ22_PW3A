836da49a8a05f4ac428036cebdca13488311ed9d
ingvega <isc.p.vega@gmail.com>
2022-04-08 09:06:02 -0500

    Integración del avance de la animación de JS

--- unidad 4/animacionJSTexto/scripts.js
+++ unidad 4/animacionJSTexto/scripts.js
@@ -2,12 +2,9 @@ window.addEventListener("load",function(){
     var config={};
     config.duration=2000;
     config.delay=500;
-    config.fills='forwards';
+    config.fill='forwards';
 
-    /*var config={
-        duration:2000,
-        delay:3000
-    };*/
+    
 
     var keyframes=[];
     var k1={},k2={};
@@ -16,19 +13,32 @@ window.addEventListener("load",function(){
     keyframes.push(k1);
     keyframes.push(k2);
 
-    /*var keyframes=[
-       {transform:'rotateX(180deg)'},
-       {transform:'rotateX(00deg)'},
-    ];*/
+    var keyframes2=[
+        {transform:'rotateY(0deg)'},
+        {transform:'rotateY(360deg)'},
+     ];
+
+     var config2={
+        duration:2000,
+        delay:500,
+        iterations:Infinity
+    };
+    
     var keyframes=[];
     var k1={},k2={};
     k1.transform='rotateX(180deg)';
-    k2.transform='rotateX(0deg)';
+    k2.transform= 'rotateX(0deg)';
     keyframes.push(k1);
     keyframes.push(k2);
 
     var letras=document.getElementsByClassName('letra');
-    for(var i=0;i<=letras.length;i++){
-        letras[i].animate(keyframes,config);
+    var retardo=500;
+    for(var i=0;i<letras.length;i++){
+        config.delay=retardo;
+        config2.delay=retardo;
+        //letras[i].animate(keyframes,config);
+        //letras[i].animate(keyframes2,config2);
+        retardo+=2000;
     }
 });
+
